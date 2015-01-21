8f349a056ac2633a76ff5aa2b83fe47c2524b0f8
Keith Casey <keith@caseysoftware.com>
2015-01-20 23:25:47 -0600

    wiring the loop of audio files

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+include 'creds.php';
 include 'vendor/autoload.php';
 
 $terms = $_GET['terms'];
@@ -8,14 +9,11 @@ $terms = preg_replace("/[^A-Za-z0-9|]/", "", $terms);
 $bundle = new Clarify\Bundle($apikey);
 $items = $bundle->search($terms);
 
+$total = (int) $items['total'];
 $search_terms = json_encode($items['search_terms']);
-$item_results = json_encode($items['item_results']);
+$item_results = $items['item_results'];
 
-$bundlekey = $items['_links']['items'][0]['href'];
-$tracks = $bundle->tracks->load($bundlekey)['tracks'];
-
-$mediaUrl = $tracks[0]['media_url'];
-$duration = $tracks[0]['duration'];
+$bundles = $items['_links']['items'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
@@ -40,50 +38,61 @@ $duration = $tracks[0]['duration'];
                 // Set the path to the jplayer swf file.
                 o3vPlayer.jPlayerOptions.swfPath = 'scripts/jquery';
 
-                // Set to the playback URL for the audio file.
-                var mediaURL = '<?php echo $mediaUrl; ?>';
-
                 ////////////////////////////////////////////////////////
-
                 // This is a sample search_terms array from a SearchCollection
                 var searchTerms = <?php echo $search_terms; ?>;
 
+                <?php foreach ($bundles as $key => $_bundle) {
+                    $bundlekey = $_bundle['href'];
+                    $tracks = $bundle->tracks->load($bundlekey)['tracks'];
+
+                    $mediaUrl = $tracks[0]['media_url'];
+                    $duration = $tracks[0]['duration'];
+                    ?>
+                // Set to the playback URL for the video file(s).
+                var mediaURLs<?php echo $key; ?> = { mp3:"<?php echo $mediaUrl; ?>"};
                 // This is a sample "ItemResult" object from a SearchCollection JSON
                 // object. It is one item in the item_results array.
-                var itemResult =  <?php echo substr($item_results, 1, -1); ?>;
-
+                var itemResult<?php echo $key; ?> =  <?php echo json_encode($item_results[$key]); ?>;
                 ////////////////////////////////////////////////////////
+
                 // Create a player and add in search results marks
-                var convDuration = <?php echo $duration; ?>;
-                var player = o3vPlayer.createPlayer("#player_instance_1", mediaURL, convDuration,{volume:0.5});
-                o3vPlayer.addItemResultMarkers(player, convDuration, itemResult, searchTerms);
+                var player = o3vPlayer.createPlayer("#player_instance_<?php echo $key; ?>", mediaURLs<?php echo $key; ?>, <?php echo $duration; ?>, {volume:0.5});
+                o3vPlayer.addItemResultMarkers(player, <?php echo $duration; ?>,itemResult<?php echo $key; ?>, searchTerms);
 
                 ////////////////////////////////////////////////////////
                 // Create words tags for SearchCollection.
+
                 for (var i=0,c=searchTerms.length;i<c;i++) {
                     var term = searchTerms[i].term;
                     var dtag = document.createElement('div');
                     $(dtag).addClass("o3v-search-tag o3v-search-color-"+i);
                     $(dtag).text(term);
-                    $("#player_1_search_tags").append(dtag);
+                    $("#player_<?php echo $key; ?>_search_tags").append(dtag);
                 }
                 dtag = document.createElement('div');
                 $(dtag).addClass("o3v-clear");
-                $("#player_1_search_tags").append(dtag);
+                $("#player_<?php echo $key; ?>_search_tags").append(dtag);
                 ////////////////////////////////////////////////////////
-
+                <?php } ?>
             });
         </script>
     
     </head>
     <body>
-        <h3>Clarify JPlayer Demo</h3>
+        <h3>Clarify JPlayer Audio Demo</h3>
+        <form action="" method="GET">
+            Search terms: <input name="terms" value="" />
+            <input type="submit" />
+        </form>
         <br>
         Player Example:
         <br>
         <em>If no audio player appears, there was not a search result found.</em>
         <br>
-        <div id="player_1_search_tags" class="o3v-search-tag-box"></div>
-        <div id="player_instance_1"></div>
+        <?php foreach ($bundles as $key => $_bundle) { ?>
+            <div id="player_<?php echo $key; ?>_search_tags" class="o3v-search-tag-box"></div>
+            <div id="player_instance_<?php echo $key; ?>"></div>
+        <?php } ?>
     </body>
 </html>
