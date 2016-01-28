43a380677b1666767e992cefcd8091ecaf531819
Keith Casey <keith@caseysoftware.com>
2016-01-28 01:26:58 -0600

    matching formatting

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $search_terms = json_encode($items['search_terms']);
 $item_results = $items['item_results'];
 
 $bundles = $items['_links']['items'];
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
